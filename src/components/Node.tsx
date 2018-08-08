f3c25874abb8d925d298615e01fcf36030406e0b
Cezary Budrewicz <34355826+Sazar24@users.noreply.github.com>
2018-08-08 12:34:39 +0200

    css defeated
    divs properly cssed (every child has its own frame + button placed next to Node List.Item)

--- src/components/Node.tsx
+++ src/components/Node.tsx
@@ -1,62 +1,66 @@
-import * as React from 'react';
-import { connect } from 'react-redux';
-import { INode, SingleNode } from '../models/Node';
+import * as React from "react";
+import { connect } from "react-redux";
+import { INode, SingleNode } from "../models/Node";
 import { List, Container } from "semantic-ui-react";
-import { v1 } from 'uuid';
-import { IState } from '../reducers';
-import NewNodeButton from './NewNodeButton';
-import ConnectedNodeFrame from './connectedComponents/ConnectedNode';
+import { v1 } from "uuid";
+import { IState } from "../reducers";
+import NewNodeButton from "./NewNodeButton";
+import ConnectedNodeFrame from "./connectedComponents/ConnectedNode";
 
 export interface IProps {
-    nodeId: string;
-    node: INode;
-    childrenIDs: string[],
+  nodeId: string;
+  node: INode;
+  childrenIDs: string[];
 }
 
-export class NodeFrame extends React.Component<IProps>{
-
-    public renderMyChilds() {
-        const { childrenIDs } = this.props;
-        if (!childrenIDs || childrenIDs.length === 0) {
-            return;
-        };
-
-        return (
-            <List>
-                {childrenIDs.map((id) => {
-                    return (
-                        <List.Item key={id}>
-                            <ConnectedNodeFrame nodeId={id} />
-                        </List.Item>
-                    )
-                })
-                }
-            </List>
-        )
+export class NodeFrame extends React.Component<IProps> {
+  public renderMyChilds() {
+    const { childrenIDs } = this.props;
+    if (!childrenIDs || childrenIDs.length === 0) {
+      return;
     }
 
-    public render(): any {
-        const { node } = this.props;
-        if (node === undefined) {
-            throw new Error(' passed undefined {node} to NodeFrame');
-        };
-
-        return (
-            <List.Item style={{ border: "1px dashed black", width: "500px", backgroundColor: "yellow" }} >
-                <div style={{ width: "500px" }}><div style={{ float: "left", padding: "0 12px", border: "0px solid red" }}>
-                    <List.Header>
-                        {node.header} ++Id: {node.Id}
-                    </List.Header>
-                    <List.Description>
-                        {node.description} | 
-                        parent: {(node.parentID) ? node.parentID : "(i dont have any parents)"}
-                    </List.Description></div>
-                    <div style={{ float: "left" }}>
-                        <NewNodeButton nodeId={this.props.nodeId} />
-                    </div>
-                    {this.renderMyChilds()}
-                </div>
+    return (
+      <List>
+        {childrenIDs.map(id => {
+          return (
+            <List.Item key={id}>
+              <ConnectedNodeFrame nodeId={id} />
             </List.Item>
-        )
+          );
+        })}
+      </List>
+    );
+  }
+
+  public render(): any {
+    const { node } = this.props;
+    if (node === undefined) {
+      throw new Error(" passed undefined {node} to NodeFrame");
     }
+
+    return (
+      <List.Item style={{ minWidth: "500px", backgroundColor: "yellow" }}>
+        <div style={{ minWidth: "500px", display: "inline" }}>
+          <div style={{ padding: "0 12px", border: "1px solid black" }} >
+            <div style={{ display: "inline-flex" }}>
+              <div >
+                <List.Header>
+                  {node.header} ++Id: {node.Id}
+                </List.Header>
+                <List.Description>
+                  {node.description} | parent:{" "}
+                  {node.parentID ? node.parentID : "(i dont have any parents)"}
+                </List.Description>
+              </div>
+              <div style={{}}>
+                <NewNodeButton nodeId={this.props.nodeId} />
+              </div>
+            </div>
+            {this.renderMyChilds()}
+          </div>
+        </div>
+      </List.Item>
+    );
+  }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './components/App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
-import { Provider } from 'react-redux';
-import store from './store/store';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./components/App";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 registerServiceWorker();
